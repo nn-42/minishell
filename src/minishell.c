ceccd83cbdd098dc59184683615c987eaf35bffc
NoorFaronia <noorfaronia2001@gmail.com>
2026-02-09 07:53:14 +0300

    minishell

--- src/minishell.c
+++ src/minishell.c
@@ -12,33 +12,59 @@
 
 #include "minishell.h"
 
-void	handle_sigint(int sig)
+void sigint_handler(int sig)
 {
-	g_signal = sig;
-	write(1, "\n", 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+    (void)sig;
+    g_signal = SIGINT;
+    write(1, "\n", 1);
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
 }
 
-int	main(void)
+void sigquit_handler(int sig)
 {
-	char	*line;
+    (void)sig;
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
+}
 
-	signal(SIGINT, handle_sigint);
-	signal(SIGQUIT, SIG_IGN);
-	while (1)
-	{
-		line = readline("minishell$ ");
-		if (!line)
-		{
-			write(1, "exit\n", 5);
-			break ;
-		}
-		if (*line)
-			add_history(line);
-		free(line);
-	}
-	rl_clear_history();
-	return (0);
+void setup_signals(void)
+{
+    struct sigaction sa_int;
+    struct sigaction sa_quit;
+
+    sa_int.sa_handler = sigint_handler;
+    sigemptyset(&sa_int.sa_mask);
+    sa_int.sa_flags = SA_RESTART;
+    sigaction(SIGINT, &sa_int, NULL);
+
+    sa_quit.sa_handler = sigquit_handler;
+    sigemptyset(&sa_quit.sa_mask);
+    sa_quit.sa_flags = SA_RESTART;
+    sigaction(SIGQUIT, &sa_quit, NULL);
+}
+
+int main(void)
+{
+    char *line;
+
+    setup_signals();
+
+    while (1)
+    {
+        line = readline("minishell$ ");
+        if (!line)
+        {
+            write(1, "exit\n", 5);
+            break;
+        }
+        if (*line)
+            add_history(line);
+        free(line);
+    }
+
+    rl_clear_history();
+    return 0;
 }
